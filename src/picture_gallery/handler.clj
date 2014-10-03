21a163b872fb0cabf20826e5009f36990e8c0126
Chris Howe-Jones <chris.howejones@gmail.com>
2014-10-03 13:23:29 +0100

    Completed user registration use case.

--- src/picture_gallery/handler.clj
+++ src/picture_gallery/handler.clj
@@ -1,11 +1,9 @@
 (ns picture-gallery.handler
-  (:require [compojure.core :refer [defroutes routes]]
-            [ring.middleware.resource :refer [wrap-resource]]
-            [ring.middleware.file-info :refer [wrap-file-info]]
-            [hiccup.middleware :refer [wrap-base-url]]
-            [compojure.handler :as handler]
+  (:require [compojure.core :refer [defroutes]]
             [compojure.route :as route]
-            [picture-gallery.routes.home :refer [home-routes]]))
+            [picture-gallery.routes.home :refer [home-routes]]
+            [noir.util.middleware :as noir-middleware]
+            [picture-gallery.routes.auth :refer [auth-routes]]))
 
 (defn init []
   (println "picture-gallery is starting"))
@@ -17,9 +15,8 @@
   (route/resources "/")
   (route/not-found "Not Found"))
 
-(def app
-  (-> (routes home-routes app-routes)
-      (handler/site)
-      (wrap-base-url)))
+(def app (noir-middleware/app-handler [auth-routes
+                                       home-routes
+                                       app-routes]))
 
 
