01268e7945f62bf9432f22759def99f31bf3fc9d
Chris Howe-Jones <chris.howejones@gmail.com>
2014-10-04 20:00:59 +0100

    Upload image functionality added.

--- src/picture_gallery/routes/upload.clj
+++ src/picture_gallery/routes/upload.clj
@@ -31,11 +31,20 @@
   (upload-page
    (if (empty? filename)
      "please select a file to upload"
-     "success")))
+     (try
+       (noir.io/upload-file (gallery-path) file :create-path? true)
+       (image {:height "150px"}
+              (str "/img/" (url-encode filename)))
+       (catch Exception ex
+         (str "error uploading file " (.getMessage ex)))))))
+
+(defn serve-file [file-name]
+  (file-response (str (gallery-path) File/separator file-name)))
 
 (defn gallery-path []
   "galleries")
 
 (defroutes upload-routes
   (GET "/upload" [info] (upload-page info))
-  (POST "/upload" [file] (handle-upload file)))
+  (POST "/upload" [file] (handle-upload file))
+  (GET "/img/:file-name" [file-name] (serve-file file-name)))
