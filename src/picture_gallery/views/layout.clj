7d7b249ed6ec772f10e2074c0ce94b1e12177a8c
Chris Howe-Jones <chris.howejones@gmail.com>
2014-10-23 15:19:20 +0100

    Changes to set colors and styles in gallery.

--- src/picture_gallery/views/layout.clj
+++ src/picture_gallery/views/layout.clj
@@ -21,13 +21,15 @@
        (include-css "/css/screen.css")
        [:script {:type "text/javascript"}
         (str "var context=\"" (:context request) "\";")]
-       (include-js "//code.jquery.com/jquery-2.0.2.min.js")]
+       (include-js "//code.jquery.com/jquery-2.0.2.min.js"
+                   "/js/colors.js"
+                   "/js/site.js")]
       [:body content]))))
 
 (defn make-menu
   "Make menu."
   [& items]
-  [:div (for [item items]
+  [:div#usermenu (for [item items]
           [:div.menuitem item])])
 
 (defn guest-menu
