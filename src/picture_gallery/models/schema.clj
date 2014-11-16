b4d67a745d8a9fbe23a81a5cb29e57afe519a824
Chris Howe-Jones <chris.howejones@gmail.com>
2014-11-16 20:58:18 +0000

    Changed schema to use function for database connection

--- src/picture_gallery/models/schema.clj
+++ src/picture_gallery/models/schema.clj
@@ -3,14 +3,14 @@
             [clojure.java.jdbc :as sql]))
 
 (defn create-users-table []
-  (sql/with-connection db
+  (sql/with-connection (-database-url)
     (sql/create-table
      :users
      [:id "varchar(32) PRIMARY KEY"]
      [:pass "varchar(100)"])))
 
 (defn create-images-table []
-  (sql/with-connection db
+  (sql/with-connection (-database-url)
     (sql/create-table
      :images
      [:userid "varchar(32)"]
