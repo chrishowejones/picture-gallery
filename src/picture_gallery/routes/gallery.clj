ee66d9aac4af71ae88b73f727bfe69d36fb7bf78
Chris Howe-Jones <chris.howejones@gmail.com>
2014-11-20 17:21:34 +0000

    Changed gallery page to use selmer template.

--- src/picture_gallery/routes/gallery.clj
+++ src/picture_gallery/routes/gallery.clj
@@ -10,40 +10,14 @@
             [picture-gallery.util :refer [gallery-path]]
             [hiccup.element :refer [link-to]]))
 
-(defn thumbnail-link
-  "Display a thumbnail as a link to the larger image."
-  [{:keys [userid name]}]
-  [:div.thumbnail
-   [:a {:href (image-uri userid name)}
-    (image (thumb-uri userid name))
-    (if (= userid (session/get :user)) (check-box name))]])
-
 (defn display-gallery
   "Display gallery for a user."
   [userid]
-  (if-let [gallery (not-empty (map thumbnail-link (db/images-by-user userid)))]
-    [:div
-     [:div#error]
-     gallery
-     (if (= userid (session/get :user))
-       [:input#delete {:type "submit" :value "delete images"}])]  
-   [:p "The user " userid " does not have any galleries"]))
-
-(defn gallery-link
-  "Display a link to the users gallery."
-  [{:keys [userid name]}]
-  [:div.thumbnail
-   (link-to (str "gallery/" userid)
-            (image (thumb-uri userid name))
-            userid "'s gallery")])
-
-(defn show-galleries
-  "Display the galleries for a user."
-  []
-  (map gallery-link (db/get-gallery-previews)))
+  (layout/render-template "gallery.html"
+                          {:thumb-prefix thumb-prefix
+                           :page-owner userid
+                           :pictures (db/images-by-user userid)}))
 
 (defroutes gallery-routes
   "Define routes to gallery"
-  (GET "/gallery/:userid" [userid] (layout/common
-                                    (include-js "/js/gallery.js")
-                                    (display-gallery userid))))
+  (GET "/gallery/:userid" [userid] (display-gallery userid)))
