dffbed7bdb1e765c17336597afc3f5a22ced2223
Chris Howe-Jones <chris.howejones@gmail.com>
2015-04-12 19:59:08 +0100

    Fixed ref to clojure.string/replace overriding clojure.core/replace

--- src/picture_gallery/util.clj
+++ src/picture_gallery/util.clj
@@ -2,7 +2,7 @@
   (:require [noir.session :as session]
             [hiccup.util :refer [url-encode]]
             [environ.core :refer [env]]
-            [clojure.string :refer [replace]])
+            [clojure.string :as str])
   (:import java.io.File))
 
 (def galleries (env :galleries-path))
@@ -17,7 +17,7 @@
 (def thumb-prefix "thumb_")
 
 (defn image-uri [userid file-name]
-  (str "/img/" userid "/" (replace (url-encode file-name) #"\+" "%20")))
+  (str "/img/" userid "/" (str/replace (url-encode file-name) #"\+" "%20")))
 
 (defn thumb-uri [userid file-name]
   (image-uri userid (str thumb-prefix file-name)))
